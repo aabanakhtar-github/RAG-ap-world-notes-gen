34cc142cab63fd6bff42986e7bf9b6d82d931d1f
aabanakhtar-github <aabanakhtar.home@gmail.com>
2025-02-09 21:44:46 -0500

    retreival done?

--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
 import getpass
 import os
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_cohere import ChatCohere 
+from langchain_cohere import ChatCohere
+from vectordb import InMemoryVectorDB 
 
 if not os.environ.get("COHERE_API_KEY"):
   os.environ["COHERE_API_KEY"] = getpass.getpass("Enter API key for Cohere: ")
   
-model = ChatCohere(model = "command-r-plus")
-history = []
-
-model.invoke(
-    [
-        HumanMessage(content="Hi! I'm Bob"),
-        AIMessage(content="Hello Bob! How can I assist you today?"),
-        HumanMessage(content="What's my name?"),
-    ]
-)
+InMemoryVectorDB("../data/random.pdf").createDB()
--- src/vectordb.py
+++ src/vectordb.py
@@ -1,8 +1,33 @@
 from langchain_community.document_loaders import UnstructuredPDFLoader 
-# create the loader 
-path = "../data/test.pdf"
-loader = UnstructuredPDFLoader(path, mode="elements") 
-pages = loader.load()
-print(f"{pages[3].metadata}")
-print("contents")
-print(f"{pages[3].page_content}")
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.documents import Document
+from langchain_cohere import CohereEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+
+class InMemoryVectorDB: 
+    '''A class to represent the note taker's files in pdf form'''
+
+    def __init__(self, path):
+        #self.loader = UnstructuredPDFLoader(path, mode = "single")
+        print("loading files....")
+        self.pages = Document(
+            page_content="Hello, world! PME is oso coool!",
+            metadata={"source": "https://example.com"}
+        )
+        print("creating vector store....")
+        embeddings = CohereEmbeddings(
+            model="embed-english-v3.0",
+        )
+        self.store = InMemoryVectorStore(embeddings)
+
+    def createDB(self):
+        # chunkviz.up.railway.app
+        splitter = CharacterTextSplitter(
+            separator = ".",
+            chunk_size= 475,
+            chunk_overlap = 50,
+            length_function = len
+        )
+        self.pages = splitter.split_documents([self.pages])
+        self.store.add_documents(self.pages) 
+        print(self.store.as_retriever().invoke("PME"))
